45e88f5fa3f25e015c297da4fb084d1be8f01806
Pulasthi Narada <pulasthinarada@hotmail.com>
2022-09-30 10:54:30 +0530

    Configure Spring Controllers

--- src/main/java/lk/oulasthi/frist/controller/BookController.java
+++ src/main/java/lk/oulasthi/frist/controller/BookController.java
@@ -1,9 +1,26 @@
 package lk.oulasthi.frist.controller;
 
+import lk.oulasthi.frist.repostories.BookRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BookController {
 
 
+    private final BookRepo bookRepository;
+
+    public BookController(BookRepo bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model){
+
+        model.addAttribute("books", bookRepository.findAll());
+
+        return "books";
+    }
 }
--- src/main/java/lk/oulasthi/frist/repostories/BookRepo.java
+++ src/main/java/lk/oulasthi/frist/repostories/BookRepo.java
@@ -2,6 +2,8 @@ package lk.oulasthi.frist.repostories;
 
 import lk.oulasthi.frist.model.Book;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Component;
+
 
 public interface BookRepo extends CrudRepository<Book,Integer> {
 }
